6ba1986aca686cd6454a4bacba8357806aa8b2cb
Code Stack <codestack.net@gmail.com>
2019-03-10 13:56:16 +1100

    Added the split body by faces feature

--- Sw/Properties/AssemblyInfo.cs
+++ Sw/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using Xarial.AppLaunchKit.Services.Attributes;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CodeStack")]
 [assembly: AssemblyProduct("Geometry++")]
-[assembly: AssemblyCopyright("Copyright © www.codestack.net 2018")]
+[assembly: AssemblyCopyright("Copyright © www.codestack.net 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -18,8 +18,8 @@ using Xarial.AppLaunchKit.Services.Attributes;
 
 [assembly: Guid("060b3bc3-0502-4903-96c8-35823ac63a50")]
 
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
 
 [assembly: UpdatesUrl(typeof(Resources), nameof(Resources.UpdateUrl))]
 
